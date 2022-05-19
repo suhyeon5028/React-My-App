d139066f60087d023904881b49d5249c4111efa8
suhyeon <suhyeon5028@naver.com>
2022-05-19 12:13:55 +0900

    styled-components(재사용 불가)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import "./App.css";
+import styledComponents from 'styled-components';
+
+const Title = styledComponents.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;  
+`; // 재사용 불가(js파일에 따로 만들면 재사용 가능)
 
 function App() {
-  return <div>안녕</div>;
+  return (
+    <div>
+      <Title>헬로</Title>
+    </div>
+  );
 }
 
 export default App;
