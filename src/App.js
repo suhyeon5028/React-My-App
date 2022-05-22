e76c928289abff83fab6a029dab72cba0ae94573
suhyeon <suhyeon5028@naver.com>
2022-05-22 22:27:09 +0900

    글쓰기 실습

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import { Route, Routes } from 'react-router-dom';
-import Navigation from './components/Navigation';
 import ListPage from './pages/ListPage';
-import WritePage from './pages/WritePage';
 
 function App() {
   return (
     <div>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<ListPage />} />
-        <Route path="/write" element={<WritePage />} />
-      </Routes>
+      <ListPage />
     </div>
   );
 }
