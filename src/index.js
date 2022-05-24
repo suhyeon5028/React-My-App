5ed2967871ab6c6e32b9c96d89f2af9b581ed600
suhyeon <suhyeon5028@naver.com>
2022-05-24 20:32:15 +0900

    react-redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import reducer from './store';
+import { Provider } from 'react-redux';
+import { createStore } from '@reduxjs/toolkit';
+
+const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Provider store={store}>
       <App />
-    </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 );
 
